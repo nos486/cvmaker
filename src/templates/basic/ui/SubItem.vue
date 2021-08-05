bfd150b30650b2651f814a83ab46cd88b403ccdf
sina <nos486@gmail.com>
2021-08-06 02:23:21 +0430

    add header

--- src/templates/basic/ui/SubItem.vue
+++ src/templates/basic/ui/SubItem.vue
@@ -1,32 +1,35 @@
 <template>
-    <div class="mb-3 ">
-        <IconText :color="color" :text="title" :icon="icon" class="text-capitalize"></IconText>
-
-        <div class="text-secondary mt-n1">
-            <slot></slot>
-        </div>
+  <div class="mb-4">
+    <div class="d-flex align-start justify-space-between">
+      <IconText :color="color" :text="title" :icon="icon" class="text-capitalize"></IconText>
+      <slot name="header"></slot>
     </div>
+    <div class="text-secondary mt-n1">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
 import IconText from "@/templates/basic/ui/IconText";
+
 export default {
-    name: "SubItem",
+  name: "SubItem",
   components: {IconText},
   props: {
-        title: {
-            type: String,
-            default: "Title"
-        },
-        icon: {
-            type: String,
-            default: ""
-        },
-      color: {
-        type: String,
-        default: "grey"
-      },
-    }
+    title: {
+      type: String,
+      default: "Title"
+    },
+    icon: {
+      type: String,
+      default: ""
+    },
+    color: {
+      type: String,
+      default: "grey"
+    },
+  }
 }
 </script>
 
