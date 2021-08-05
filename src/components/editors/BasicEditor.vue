d3f5dbd05bb96eaa69fe1e78bb9b8a08fa9d83b1
sina <nos486@gmail.com>
2021-08-06 02:25:53 +0430

    add avatar upload

--- src/components/editors/BasicEditor.vue
+++ src/components/editors/BasicEditor.vue
@@ -12,7 +12,14 @@
 
         <v-card-text class="py-4 px-4 px-sm-8">
 
+
           <v-form ref="form" v-model="isFormValid">
+            <div class="d-flex align-center">
+              <v-avatar>
+                <AvatarImage :id="$store.getters.user.id" size="60"></AvatarImage>
+              </v-avatar>
+              <v-file-input v-model="uploadAvatar" class="ml-2" accept="image/*" label="Avatar picture" color="cyan"></v-file-input>
+            </div>
             <div class="d-flex">
               <v-text-field v-model="firstName" label="First Name" prepend-inner-icon="mdi-account-details"
                             color="cyan"></v-text-field>
@@ -51,16 +58,18 @@
 import UserModel from "@/models/User.model";
 import IconSelector from "@/components/IconSelector";
 import DateSelector from "@/components/DateSelector";
+import AvatarImage from "@/components/ui/AvatarImage";
 
 export default {
   name: 'BasicEditor',
-  components: {DateSelector, IconSelector},
+  components: {AvatarImage, DateSelector, IconSelector},
   data: () => ({
     isShow: false,
     isFormValid: false,
+    uploadAvatar : null,
     firstName: "",
     lastName: "",
-    birthday: new Date(),
+    birthday: null,
     headLine: "",
     icon: "",
   }),
@@ -92,9 +101,21 @@ export default {
         icon: this.icon,
       }
 
+      console.log(this.uploadAvatar)
+
       this.$store.dispatch("updateUserData", data).then(() => {
         this.$toast.success("User updated.")
-        this.hide()
+
+        if(this.uploadAvatar != null){
+          let formData = new FormData()
+          formData.append("avatar",this.uploadAvatar)
+          this.$store.dispatch("uploadAvatar", formData).then(()=>{
+            this.$toast.success("Avatar updated.")
+            this.hide()
+          })
+        }else {
+          this.hide()
+        }
       })
     }
   }
