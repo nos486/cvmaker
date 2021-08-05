f60d3da5912ce26d8a75fe3247125a29af0474ac
sina <nos486@gmail.com>
2021-08-06 02:26:51 +0430

    add edit

--- src/templates/basic/Languages.vue
+++ src/templates/basic/Languages.vue
@@ -1,15 +1,19 @@
 <template>
     <item v-if="user.languages.length > 0" title="Languages" :color="user.settings.color">
+      <template v-slot:edit>
+        <LanguageEditor :user="user"></LanguageEditor>
+      </template>
         <li v-for="(language,index) in user.languages" :key="index">{{ language }}</li>
     </item>
 </template>
 <script>
 import Item from "@/templates/basic/ui/Item"
 import UserModel from "@/models/User.model";
+import LanguageEditor from "@/components/editors/LanguageEditor";
 
 export default {
     name: 'Languages',
-    components: {Item},
+    components: {LanguageEditor, Item},
     props: {
         user: {
             type: UserModel
