9aa20c3caac235fa7f0ab591098a1aabbe717c38
Sina <nos486@gmail.com>
2021-11-02 20:22:50 +0330

    fix ui

--- src/templates/default/ui/IconText.vue
+++ src/templates/default/ui/IconText.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex align-center font-weight-light  mb-1" style="font-size: 17px">
-    <v-icon v-if="icon !== ''" left :color="color+' darken-1'">{{ icon }}</v-icon>
+    <v-icon v-if="!(icon==='mdi-blank' || icon === '')" left :color="color+' darken-1'">{{ icon }}</v-icon>
     <div v-if="href">
       <a :href="'//'+href" :class="color+'--text'" class="text--darken-2"
          style="text-decoration: none;"><b>{{ text }}</b></a>
